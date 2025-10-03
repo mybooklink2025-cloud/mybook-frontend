a60499c7b29fe726a26a2d741aab15a7e4f7f01d
Admin <mybook.link.2025@gmail.com>
2025-10-02 20:50:39 -0500

    Actualizar App.js con rutas para login y registro

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./login";
+import Register from "./register";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1 style={{ color: "blue" }}>MyBook</h1>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/">Iniciar sesión</Link> |{" "}
+          <Link to="/register">Registrarse</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
