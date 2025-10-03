429350d6d283f2a048dc4cee2a60651f60ccab7f
Admin <mybook.link.2025@gmail.com>
2025-10-02 22:11:08 -0500

    corregir api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const registrarUsuario = async (usuarioData) => {
-  const res = await fetch(`${backendUrl}/api/users/registro`, {
+  const res = await fetch(`${backendUrl}/api/usuarios/registro`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(usuarioData)
@@ -10,7 +10,7 @@ export const registrarUsuario = async (usuarioData) => {
 };
 
 export const loginUsuario = async (loginData) => {
-  const res = await fetch(`${backendUrl}/api/users/login`, {
+  const res = await fetch(`${backendUrl}/api/usuarios/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(loginData)
